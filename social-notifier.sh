9a1ccf327051a87900f6ed6779de8e5c4efc77fb
JaSpr <jaspradlin@gmail.com>
2012-03-30 23:35:12 -0500

    removed duplicates

--- social-notifier.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-~/social-notifier/application.rb start
-sleep 5
-~/social-notifier/application.rb add twitter home
-sleep 5
-~/social-notifier/application.rb add twitter list the_nerdery our-nerds
